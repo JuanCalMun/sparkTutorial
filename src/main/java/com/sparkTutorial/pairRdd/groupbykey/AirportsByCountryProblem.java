04e55343719c893cd7c4f597fdf859d3ab6817cd
JuanCalMun <juancalmun@gmail.com>
2020-11-18 13:48:17 +0100

    AirportsByCountryProblem Solved

--- src/main/java/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.java
+++ src/main/java/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.java
@@ -1,8 +1,17 @@
 package com.sparkTutorial.pairRdd.groupbykey;
 
+import com.sparkTutorial.rdd.commons.Utils;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
 public class AirportsByCountryProblem {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         /* Create a Spark program to read the airport data from in/airports.text,
            output the the list of the names of the airports located in each country.
@@ -18,5 +27,20 @@ public class AirportsByCountryProblem {
            "Papua New Guinea",  ["Goroka", "Madang", ...]
            ...
          */
+
+        Logger.getLogger("org").setLevel(Level.ERROR);
+
+        SparkConf sparkConf = new SparkConf().setAppName("airportsByCountryProblem").setMaster(Utils.MASTER_LOCAL);
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+        JavaRDD<String> lines = sparkContext.textFile("in/airports.text");
+        JavaPairRDD<String, Iterable<String>> airportsByCountry =
+                lines.map(line -> line.split(Utils.COMMA_DELIMITER))
+                        .mapToPair(columns -> new Tuple2<>(columns[3], columns[1]))
+                        .groupByKey();
+
+        airportsByCountry.foreach((e) -> System.out.println(e._1 + ": " + e._2().toString()));
+
+
     }
 }
